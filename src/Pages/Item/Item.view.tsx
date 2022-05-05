be9027c1f6b7199e122f9678bbc1bef0484e2662
May Salmon <may@moovex.ai>
2022-05-05 18:12:00 +0300

    fixed code

--- src/Pages/Item/Item.view.tsx
+++ src/Pages/Item/Item.view.tsx
@@ -7,6 +7,7 @@ import Card from "../../views/Card/Card";
 
 import { ItemDummyData } from "../Home/Home.model";
 import classes from "./Item.module.scss";
+import logo from "../../assets/images/profero-logo.jpg";
 
 type Props = {
   tempData: ItemDummyData;
@@ -18,39 +19,44 @@ const ItemView: React.FC<Props> = (props: React.PropsWithChildren<Props>) => {
 
   return (
     <div className={classes["container"]}>
-      <Link type="button" color="primary" to="/">
-        {t("pages.item.back")}
-      </Link>
-      <Card className={classes["content"]}>
-        <div>
-          <div className={classes["attribute"]}>
-            <span className={classes["attribute__title"]}>
-              {t("pages.item.cards.id")}
-            </span>
-            <span className={classes["attribute__content"]}>
-              {props.tempData?.id}
-            </span>
+      <div className={classes["header"]}>
+        <Link type="button" color="primary" to="/">
+          {t("pages.item.back")}
+        </Link>
+        <img className={classes["logo"]} src={logo} alt="profero"></img>
+      </div>
+      <div className={classes["body"]}>
+        <Card className={classes["content"]}>
+          <div className={classes["card"]}>
+            <div className={classes["attribute"]}>
+              <span className={classes["attribute__title"]}>
+                {t("pages.item.cards.id")}
+              </span>
+              <span className={classes["attribute__content"]}>
+                {props.tempData?.id}
+              </span>
+            </div>
+            <div className={classes["attribute"]}>
+              <span className={classes["attribute__title"]}>
+                {t("pages.item.cards.url")}
+              </span>
+              <span className={classes["attribute__content"]}>
+                {props.tempData?.url}
+              </span>
+            </div>
           </div>
-          <div className={classes["attribute"]}>
-            <span className={classes["attribute__title"]}>
-              {t("pages.item.cards.url")}
-            </span>
-            <span className={classes["attribute__content"]}>
-              {props.tempData?.url}
-            </span>
-          </div>
-        </div>
-        <a href={props.tempData.url}>
-          <Button
-            className={classes["downloadBtn"]}
-            type="button"
-            color="primary"
-            onClick={props.onClickDownload}
-          >
-            {t("pages.item.download")}
-          </Button>
-        </a>
-      </Card>
+          <a href={props.tempData.url}>
+            <Button
+              className={classes["downloadBtn"]}
+              type="button"
+              color="primary"
+              onClick={props.onClickDownload}
+            >
+              {t("pages.item.download")}
+            </Button>
+          </a>
+        </Card>
+      </div>
     </div>
   );
 };
